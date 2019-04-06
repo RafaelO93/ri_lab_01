28dbcdeab055cdac4e2eea9df32d2771a0426bb6
Rafael Oliveira <rafael.santos@ccc.ufcg.edu.br>
2019-04-05 21:41:48 -0300

    final spider

--- ri_lab_01/spiders/gazeta_do_povo.py
+++ ri_lab_01/spiders/gazeta_do_povo.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
-
+from datetime import datetime
 from ri_lab_01.items import RiLab01Item
-from ri_lab_01.items import RiLab01CommentItem
 
 
 class GazetaDoPovoSpider(scrapy.Spider):
@@ -18,31 +17,38 @@ class GazetaDoPovoSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
-        for article in response.css("article"):
-            link = article.css("div.c-box-description h2 a::attr(href)").extract_first()
+        secoes = ["republica", "mundo", "educacao", "economia", "republica/2/"]
+        for secao in secoes:
+            yield response.follow(response.url + secao, self.parse_per_section)
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
 
-            yield response.follow(link, self.test)
-            page = response.url.split("/")[-2]
-            filename = 'quotes-%s.html' % page
-            with open(filename, 'wb') as f:
-                f.write(response.body)
-            self.log('Saved file %s' % filename)
-
-    def test(self, response):
+    def parse_per_page(self, response):
         subtitle = response.css("div.c-overhead ::text").extract_first()
         title = response.css("h1.c-title ::text").extract_first().encode("utf-8")
         date = response.css("div.c-credits li:nth-child(3) ::text").extract_first()
         author = response.css("div.c-credits li:nth-child(1) ::text").extract_first()
         section = response.css("li.c-title-content a ::text").extract_first()
+
         url = response.url
-        if date is None:
+        if date is None or date[0] != "[":
             date = response.css("div.c-credits li:nth-child(2) ::text").extract_first()
 
+        date = datetime.strptime(str(date[1: 11]), '%d/%m/%Y')
+
         all_p = response.css("div.paywall-google p ::text")
-        text = ''
+        text = ""
         for p in all_p:
             text += p.extract().encode("utf-8")
-
-        itemLab = RiLab01Item(title=title, author=author, url=url, sub_title=subtitle, date=date, section=section,
+        text = text.replace(',', '')
+        itemLab = RiLab01Item(title=title, author=author, url=url, sub_title=subtitle.capitalize(), date=date, section=section,
                               text=text)
         yield itemLab
+
+    def parse_per_section(self, response):
+        for article in response.css("article"):
+            link = article.css("div.c-box-description h2 a::attr(href)").extract_first()
+            yield response.follow(link, self.parse_per_page)
